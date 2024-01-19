8b23f0f25ab51bb3675cd1d6aa19d3253f5bb263
spbovarnick <spbovarnick@gmail.com>
2024-01-18 18:48:28 -0800

    scss compiling, first two components rendering

---
+++ src/app/components/HomeComponent.jsx
@@ -0,0 +1,22 @@
+// import BannerHome from "./BannerHome";
+// import TextBlock from "./TextBlock";
+// import NewsBlock from './NewsBlock';
+// import Footer from "./Footer"
+import Nav from "./Nav"
+import '../styles/util.scss'
+
+const HomeComponent = () => {
+    const leftText = "<p>Through community events and educational programming, we amplify regional contributions to arts and culture. Our media archive highlights the cultural heritage of Portland's historic Albina District.</p>";
+
+    return (
+        <div className="page-wrapper">
+            <Nav />
+            {/* <BannerHome themeLight={true} alignLeft={true} />
+            <TextBlock themeLight={false} heading="What we do" leftText={leftText} />
+            <NewsBlock/>
+            <Footer /> */}
+        </div >
+    );
+};
+
+export default HomeComponent;
---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,74 @@
+'use client'
+import { useState, useEffect } from 'react';
+// import { useLocation } from "react-router-dom";
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import classNames from 'classnames';
+import '../styles/global-nav.scss';
+
+const Nav = (props) => {
+
+    // const [locationPath, setLocationPath] = useState();
+    // const [activeLink, setActiveLink] = useState();
+    const [isOpen, setIsOpen] = useState(false);
+    // const location = useLocation();
+    const pathname = usePathname();
+
+    // Create array of refs for links
+    const numberOfLinks = 4;
+    const linkRefs = [];
+
+    // for (let i = 0; i < numberOfLinks; i++) {
+    //     const newRef = React.createRef();
+    //     linkRefs.push(newRef);
+    // }
+
+    // useEffect(() => {
+    //     const currentPath = location.pathname;
+    //     setLocationPath(currentPath);
+    // }, [location]);
+
+    // useEffect(() => {
+    //     linkRefs.forEach((link, index) => {
+    //         const linkHref = link.current.getAttribute('href');
+    //         if (linkHref === locationPath) {
+    //             setActiveLink(index);
+    //         }
+    //     });
+    // }, [locationPath]);
+
+    const cmptClasses = classNames({
+        'global-nav-wrapper': true,
+        '--is-light': props.themeLight,
+        '--is-dark': !props.themeLight,
+        '--is-open': isOpen,
+    });
+
+    return (
+        <>
+            <div className="mobile-navbar">
+                <Link className="mobile-navbar__title" href="/">WORLD ARTS FOUNDATION INC.</Link>
+                <button className="mobile-navbar__button" onClick={()=>{setIsOpen(!isOpen)}}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+
+            <div className={cmptClasses}>
+                <nav className='global-nav'>
+                    <Link className="global-nav__home-link" href="/">WORLD ARTS FOUNDATION INC.</Link>
+                    <ul className="global-nav__links">
+                        <li><Link className={pathname === "/about" ? '--is-active' : ''} href="/about">About</Link></li>
+                        <li><Link className={pathname === "/archive" ? '--is-active' : ''} href="/archive">Cultural Archive</Link></li>
+                        <li><Link className={pathname === "/our-work" ? '--is-active' : ''} href="/our-work">Our Work</Link></li>
+                        <li><Link className={pathname === "/leadership" ? '--is-active' : ''} href="/leadership">Leadership</Link></li>
+                    </ul>
+                    <Link className="global-nav__donate-link" href="/donate">Donate</Link>
+                </nav>
+            </div>
+        </>
+    );
+}
+
+export default Nav;
