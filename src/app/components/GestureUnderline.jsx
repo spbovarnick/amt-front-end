7cd8a439a6c54b8cc8fc58cf258ba6ff4124319b
spbovarnick <spbovarnick@gmail.com>
2024-02-05 19:37:38 -0800

    gesture rendering

--- src/app/components/GestureUnderline.jsx
+++ src/app/components/GestureUnderline.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import classNames from 'classnames';
+import "../styles/gesture-underline.scss";
+import underline from '../../../public/images/gesture-01-purple.svg'
 
 const GestureUnderline = ({ text, url, isLight, isSmall, isDonate, onClickFn, isExternal }) => {
   const cmptClasses = classNames({
@@ -21,8 +23,10 @@ const GestureUnderline = ({ text, url, isLight, isSmall, isDonate, onClickFn, is
 
       {!onClickFn && !isExternal &&
         <Link
+          prefetch={false}
           className={cmptClasses}
           href={url}
+          style={{ background: `url(${underline}) center bottom no-repeat`}}
         >
           {text}
         </Link>
