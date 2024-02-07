6c6b9f0c530dee64a0d91df6489ad8687cfde677
spbovarnick <spbovarnick@gmail.com>
2024-02-07 15:17:41 -0800

    img optimization disabled, react modal added to package.json

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/app/styles')],
   },
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
