8877e2b07e032d2f8b80dc52811458f255fbf326
spbovarnick <spbovarnick@gmail.com>
2024-02-07 15:16:57 -0800

    last home components added

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+
+import icon_fb from "../../../public/images/icons/fb.svg"
+import icon_insta from '../../../public/images/icons/insta.svg';
+import icon_youtube from '../../../public/images/icons/youtube.svg';
+
+const Footer = ({ archive = false, mailListLink = null, mailListName = null }) => {
+
+    return (
+        <div className="cmpt-footer">
+            <div className="global-container">
+                {!archive &&
+                    <div className="cmpt-footer__left">
+                        <div className="cmpt-footer__byline">
+                            World Arts Foundation Inc.
+                            <div className="cmpt-footer__contact">
+                                <a href="mailto:wafpdx@gmail.com">wafpdx@gmail.com</a>
+                            </div>
+                            <div className="cmpt-footer__contact">
+                                PO Box 12384 Portland,<br/>OR 97212-0384
+                            </div>
+                        </div>
+                        <div className="cmpt-footer__social">
+                            <a href="https://www.facebook.com/wafpdx" target="_blank" rel="noopener noreferrer"><img src={icon_fb.src} /></a>
+                            <a href="https://www.instagram.com/wafpdx/" target="_blank" rel="noopener noreferrer"><img src={icon_insta.src} /></a>
+                            <a href="https://www.youtube.com/channel/UC-4PwMXuB7MaLiWg55w4ldg" target="_blank" rel="noopener noreferrer"><img src={icon_youtube.src} /></a>
+                        </div>
+                    </div>
+                }
+
+                <div className="cmpt-footer__right">
+                    <ul className="cmpt-footer__links">
+                        { !archive &&
+                            <>
+                            <li><Link href="/about">About</Link></li>
+                            <li><Link href="/albina-community-archive">Cultural Archive</Link></li>
+                            <li><Link href="/our-work">Our Work</Link></li>
+                            <li><Link href="/leadership">Leadership</Link></li>
+                            <li><Link href="/donate">Donate</Link></li>
+                            </>
+                        }
+                        <li><Link href="/albina-community-archive/terms-of-use">Archive Terms of Use</Link></li>
+                        <li>To share feedback, take this <a href="https://docs.google.com/forms/d/e/1FAIpQLSea4m8mK3u4nRL3zEpbLDXWZBadYa_dIpwFE66Mjjd-4SpQaA/viewform?usp=sharing" target="_blank" className="--highlight">survey</a></li>
+                        {mailListLink && (
+                            <li>Join {mailListName ? `the ${mailListName}` : "our"} <a href={mailListLink} className="--highlight" target="_blank">mailing list</a></li>
+                        )}
+                    </ul>
+                    <span className="cmpt-footer__copy">&copy; 2021 World Arts Foundation Inc.</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/app/components/PopUp.jsx
@@ -0,0 +1,45 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import dreamFlyer from '../../../public/images/dream-flyer-updated.jpeg';
+
+const PopUp = () => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsOpen(true);
+        }, 2000);
+    }, []);
+
+    useEffect(() => {
+        Modal.setAppElement(document.getElementsByClassName('page-wrapper')[0]);
+    },[])
+
+
+    return (
+        <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                className="modal"
+                overlayClassName="overlay"
+                contentLabel="Give Guide"
+            >
+
+                <button className="modalClose" onClick={closeModal}></button>
+                <div className="modalContent give-guide-wrapper">
+                    <div className="give-guide-container">
+                        <div className="give-guide-text">
+                            <img className="dream-image" src={dreamFlyer.src} />
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+    )
+}
+
+export default PopUp;
