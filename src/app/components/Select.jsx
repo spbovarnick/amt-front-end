ef8ad7b8ee824bab90dab2ea7aed2a43f085a8cb
spbovarnick <spbovarnick@gmail.com>
2024-02-09 11:25:59 -0800

    Select component moved to dedicated UI

---
+++ src/app/components/Select.jsx
@@ -0,0 +1,23 @@
+import Select from 'react-select';
+import { mediumOptions, yearOptions } from '@/utils/api';
+
+// select moved to its own component to avoid ssr/csr conflicts from within albina-community-archive/page.js
+const SelectUI = ({ val, placeholderText, changeHandler, year=false, medium=false  }) => {
+  const options = year ? yearOptions : mediumOptions;
+
+  return (
+    <>
+    <Select
+        placeholder={placeholderText}
+        value={val}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        options={options}
+        isSearchable={false}
+        onChange={changeHandler}
+    />
+    </>
+  )
+}
+
+export default SelectUI;
