78933b82f6f929991665b4f49ba7502df1c02b19
spbovarnick <spbovarnick@gmail.com>
2024-02-20 12:30:42 -0800

    added loclhost name

--- next.config.js
+++ next.config.js
@@ -4,29 +4,22 @@ const path = require('path');
 const nextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'amt-admin-727b777103da.herokuapp.com',
-      //   port: '',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'world-arts-prod.s3.us-west-2.amazonaws.com',
-      //   port: '',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'd3qyzflaqh7cpk.cloudfront.net',
-      //   port: '',
-      // },
-      // {
-      //   protocol: 'http',
-      //   hostname: '',
-      //   port: '3000',
-      // },
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'amt-admin-727b777103da.herokuapp.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'world-arts-prod.s3.us-west-2.amazonaws.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'd3qyzflaqh7cpk.cloudfront.net'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000'
       }
     ]
   },
