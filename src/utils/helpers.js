f73ef1ddcc6281f76fa203b7cc3dc0f025b20ba4
spbovarnick <spbovarnick@gmail.com>
2024-02-19 23:55:20 -0800

    NEXT_PUBLIC prefix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,11 +8,9 @@ export function getCloudfrontUrl(
 ) {  
   // takes an activeRecord attachment path and returns a Cloudfront URL
   const url = new URL(path);
-  if (process.env.NODE_ENV === "production") {
-    console.log("production block hit in getCloudfrontUrl");
-    console.log("process.env.CLOUDFRONT_DISTRO", process.env.CLOUDFRONT_DISTRO);
+  if (process.env.NEXT_PUBLIC_TARGET_DB === "production") {
     const request = {
-      bucket: process.env.S3_BUCKET, // S3 bucket name
+      bucket: process.env.NEXT_PUBLIC_S3_BUCKET, // S3 bucket name
       key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
       edits: {
         webp: true,
@@ -27,7 +25,7 @@ export function getCloudfrontUrl(
 
     const strRequest = JSON.stringify(request);
     const encRequest = btoa(strRequest);
-    const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
+    const newUrl = `${process.env.NEXT_PUBLIC_PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
 
     return newUrl;
   } else if (process.env.TARGET_DB === "staging") {
