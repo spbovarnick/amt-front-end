6864ac2abc3d529c5745d968a21a1e40f88898cb
spbovarnick <spbovarnick@gmail.com>
2024-02-20 12:25:52 -0800

    wildcarding remotePatterns

--- next.config.js
+++ next.config.js
@@ -4,25 +4,29 @@ const path = require('path');
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'amt-admin-727b777103da.herokuapp.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'world-arts-prod.s3.us-west-2.amazonaws.com',
-        port: '',
-      },
+      // {
+      //   protocol: 'https',
+      //   hostname: 'amt-admin-727b777103da.herokuapp.com',
+      //   port: '',
+      // },
+      // {
+      //   protocol: 'https',
+      //   hostname: 'world-arts-prod.s3.us-west-2.amazonaws.com',
+      //   port: '',
+      // },
       // {
       //   protocol: 'https',
       //   hostname: 'd3qyzflaqh7cpk.cloudfront.net',
       //   port: '',
       // },
+      // {
+      //   protocol: 'http',
+      //   hostname: '',
+      //   port: '3000',
+      // },
       {
-        protocol: 'http',
-        hostname: '',
-        port: '3000',
+        protocol: 'https',
+        hostname: '**',
       }
     ]
   },
