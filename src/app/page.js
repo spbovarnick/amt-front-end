ad97536f04da959e37b06cce82f1783b0b827b07
spbovarnick <spbovarnick@gmail.com>
2024-02-26 19:05:15 -0800

    removing oG field from metadata obj

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,13 @@ export const metadata = {
   title: "Albina Community Archive",
   description: "Welcome to the Albina Community Archive, a digital repository documenting Albina's arts and culture legacy. Our mission is to engage community members and mission-aligned organizations to preserve digital versions of the Albina community's historical materials.",
   metadataBase: new URL("https://albinacommunityarchive.org"),
-  openGraph: {
-    images: [
-      {
-        url: 'https://albinacommunityarchive.org/public/images/open-graph-img.jpg'
-      }
-    ]
-  },
+  // openGraph: {
+  //   images: [
+  //     {
+  //       url: 'https://albinacommunityarchive.org/public/images/open-graph-img.jpg'
+  //     }
+  //   ]
+  // },
 };
 
 export default async function Page(){
