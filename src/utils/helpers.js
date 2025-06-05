bcaeda13685eb4803c9b6143ec45ba74350cd17d
spbovarnick <spbovarnick@gmail.com>
2025-06-05 15:12:18 -0700

    update cloudfrontUrl func

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,47 +6,70 @@ export function getCloudfrontUrl(
 ) {
   // takes an activeRecord attachment path and returns a Cloudfront URL
   const url = new URL(path);
-  if (process.env.NEXT_PUBLIC_TARGET_DB === "production") {
-    const request = {
-      bucket: process.env.S3_BUCKET, // S3 bucket name
-      key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
-      edits: {
-        webp: true,
-        jpeg: true,
-        resize: {
-          width: width,
-          height: height,
-          fit: fit,
-        },
-      },
-    };
-
-    const strRequest = JSON.stringify(request);
-    const encRequest = btoa(strRequest);
-    const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
-
-    return newUrl;
-  } else if (process.env.NEXT_PUBLIC_TARGET_DB === "staging") {
-    const request = {
-      bucket: process.env.S3_BUCKET, // S3 bucket name
-      key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
-      edits: {
-        webp: true,
-        jpeg: true,
-        resize: {
-          width: width,
-          height: height,
-          fit: fit,
-        },
-      },
-    };
-
-    const strRequest = JSON.stringify(request);
-    const encRequest = btoa(strRequest);
-    const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
-
-    return newUrl;
+  if (process.env.NEXT_PUBLIC_TARGET_DB === "development") {
+    return path
   } else {
-    return path;
+    const request = {
+      bucket: process.env.S3_BUCKET, // S3 bucket name
+      key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
+      edits: {
+        webp: true,
+        jpeg: true,
+        resize: {
+          width: width,
+          height: height,
+          fit: fit,
+        },
+      },
+    };
+
+    const strRequest = JSON.stringify(request);
+    const encRequest = btoa(strRequest);
+    const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
+
+    return newUrl;
   }
+  // if (process.env.NEXT_PUBLIC_TARGET_DB === "production") {
+  //   const request = {
+  //     bucket: process.env.S3_BUCKET, // S3 bucket name
+  //     key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
+  //     edits: {
+  //       webp: true,
+  //       jpeg: true,
+  //       resize: {
+  //         width: width,
+  //         height: height,
+  //         fit: fit,
+  //       },
+  //     },
+  //   };
+
+  //   const strRequest = JSON.stringify(request);
+  //   const encRequest = btoa(strRequest);
+  //   const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
+
+  //   return newUrl;
+  // } else if (process.env.NEXT_PUBLIC_TARGET_DB === "staging") {
+  //   const request = {
+  //     bucket: process.env.S3_BUCKET, // S3 bucket name
+  //     key: url.pathname.replace(/^\//, ""), // path to your file on the S3 bucket (ex. photos/face1.png)
+  //     edits: {
+  //       webp: true,
+  //       jpeg: true,
+  //       resize: {
+  //         width: width,
+  //         height: height,
+  //         fit: fit,
+  //       },
+  //     },
+  //   };
+
+  //   const strRequest = JSON.stringify(request);
+  //   const encRequest = btoa(strRequest);
+  //   const newUrl = `${process.env.PROD_CLOUDFRONT_DISTRO}/${encRequest}`;
+
+  //   return newUrl;
+  // } else {
+  //   return path;
+  // }
 }
