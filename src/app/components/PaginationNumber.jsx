24d7fadd240dda747e7829bd9926ba9a8c33b6a5
spbovarnick <spbovarnick@gmail.com>
2025-07-24 22:23:25 -0700

    disable ellipsis

--- src/app/components/PaginationNumber.jsx
+++ src/app/components/PaginationNumber.jsx
@@ -4,12 +4,13 @@ export default function PaginationNumber ({
   page,
   href,
   isActive,
+  isDisabled
 }) {
 
   return (
     <Link
       key={page}
-      className={`numEl ${isActive ? "current_page" : ""}`}
+      className={`numEl ${isActive ? "current_page" : ""} ${isDisabled ? "ellipsis" : ""}`}
       id={page}
       href={href}
     >
