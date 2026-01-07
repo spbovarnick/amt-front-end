7244c90efe07c39f9ab7201035b5efb663b0ef40
spbovarnick <spbovarnick@gmail.com>
2026-01-07 13:28:55 -0800

    desktop footer styled and ready

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,24 +6,40 @@ const Footer = ({  }) => {
 
     return (
         <footer>
-            <div className='footer-presentedBy'>
-                <div>PRESENTED BY</div>
-                <Image
-                    src={footerLogo}
-                    alt='AMT Logo'
-                    className='footer-logo'
-                />
-            </div>
-            <div className='footer-newsletter'>
-
-            </div>
-            <div className='footer-links'>
-                <ul>
-                    <li><Link href={"https://www.albinamusictrust.com/"}>Albina Music Trust Main Site</Link></li>
-                    <li><Link href={"/about#collections-development-policy"}>Collections Development Policy</Link></li>
-                    <li><Link href={"/about#terms-of-use"}>Terms & Conditions</Link></li>
-                    <li><Link href={"/about#privacy-policy"}>Privacy Policy</Link></li>
-                </ul>
+            <div className='footer-info'>
+                <div className='footer-presentedBy'>
+                    <div>PRESENTED BY</div>
+                    <Image
+                        src={footerLogo}
+                        alt='AMT Logo'
+                        className='footer-logo'
+                    />
+                </div>
+                <div className='footer-donate'>
+                    <div className='donate-text'>
+                        We are a 501(c)(3) non-profit based in Portland, OR<br/>
+                        EIN: 99-3975378
+                    </div>
+                    <div className='donate-btn-div'>
+                        <button className='pill-btn'>Donate</button>
+                    </div>
+                </div>
+                <div className='footer-newsletter'>
+                    <div className='newsletter-text'>
+                        Sign up for our newsletter
+                    </div>
+                    <div className='signup-btn-div'>
+                        <Link className='signup-btn pill-btn' href={"https://albinamusictrust.us14.list-manage.com/subscribe/post"}>Sign Up</Link>
+                    </div>
+                </div>
+                <div className='footer-links'>
+                    <ul>
+                        <li><Link href={"https://www.albinamusictrust.com/"}>Albina Music Trust Main Site</Link></li>
+                        <li><Link href={"/about#collections-development-policy"}>Collections Development Policy</Link></li>
+                        <li><Link href={"/about#terms-of-use"}>Terms & Conditions</Link></li>
+                        <li><Link href={"/about#privacy-policy"}>Privacy Policy</Link></li>
+                    </ul>
+                </div>
             </div>
             <div className='footer-copyright'>© Albina Community Achive 2025</div>
         </footer>
