942d6d3d5151e6399225fb13ac4e5aa0f29f9d0a
spbovarnick <spbovarnick@gmail.com>
2026-04-01 16:24:00 -0700

    search filtering. wip: clear search term

--- src/app/components/Drawer.jsx
+++ src/app/components/Drawer.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import DrawerButton from "./DrawerButton";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import xIcon from "@/../public/images/x.svg"
 import chevronDown from 'public/images/chevron-down.svg'
 import chevronUp from 'public/images/chevron-up.svg'
 import Image from "next/image";
@@ -21,10 +22,29 @@ const Drawer = ({
     const selected = sp.getAll(filterCateogry)
 
     const [isOpen, setIsOpen] = useState(false);
+    const [filteredData, setFilteredData] = useState(data)
 
     const onToggle = (name) => {
         const next = toggleFilterParam(sp, filterCateogry, name)
         router.push(`${pathname}?${next}`, { scroll: false })
+    }
+
+    const filterButtons = (event) => {
+        const wrapperEl = event.target.parentNode.parentNode;
+        console.log(wrapperEl)
+        const btns = wrapperEl.querySelectorAll(".cmpt-drawer-button")
+
+        Array.from(btns).forEach((btn) => {
+            if (
+                btn.textContent
+                    .toLowerCase()
+                    .includes(event.target.value.toLowerCase())
+            ) {
+                btn.style.display = "block";
+            } else {
+                btn.style.display = "none";
+            }
+        })
     }
 
     return (
@@ -40,7 +60,25 @@ const Drawer = ({
             </div>
             <div className={`drawer-outer button-scroll ${isOpen ? "open-drawer" : ""}`}>
                 <div className="drawer-inner">
-                    {data.map((item, i) => (
+                    { label !== "Year" && label !== "Media" &&
+                        <form className="search_assoc-data">
+                            <input
+                                className="drawer-search"
+                                placeholder={`Search for ${label.toLowerCase()}`}
+                                onChange={e => filterButtons(e)}
+                            />
+                            <button className="drawer-search-clear">
+                                <Image
+                                    src={xIcon.src}
+                                    width={20}
+                                    height={20}
+                                    alt="X icon to clear search terms"
+                                    onClick={e => clearTerm(e)}
+                                />
+                            </button>
+                        </form>
+                    }
+                    {filteredData.map((item, i) => (
                         <DrawerButton
                             key={item.id + item.name}
                             item={item}
