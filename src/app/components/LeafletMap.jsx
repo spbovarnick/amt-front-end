312b20735bf781fb9171fb45e1f9d67ddb89f5ab
spbovarnick <spbovarnick@gmail.com>
2026-06-18 14:33:00 -0700

    clusters running smoothly in inset map, popups linking, pushing to staging

--- src/app/components/LeafletMap.jsx
+++ src/app/components/LeafletMap.jsx
@@ -1,59 +1,34 @@
 "use client";
 
-import { useRef, useMemo } from "react";
-
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMemo } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "@/app/styles/map.scss";
 
-import L from "leaflet"
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
-import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import L from "leaflet";
+import redMarker from "public/images/icons/map-marker.png";
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIcon2x,
-  shadowUrl: markerShadow,
-});
-
+import Clusters from "./Clusters";
+import CenteredMarker from "./CenteredMarker";
 
 const LeafletMap = ({ centeredLoc, allLocs }) => {
-  const markerRef = useRef();
-
-  const eventHandlers = useMemo(
-    () => ({
-      mouseover() {
-        if (markerRef.current) markerRef.current.openPopup();
-      },
-      mouseout() {
-        if (markerRef.current) markerRef.current.closePopup();
-      },
-      click() {
-        if (markerRef.current) markerRef.current.togglePopup();
-      }
-    }),
-    []
-  )
+  const centeredLocIcon = useMemo(() => L.icon({
+    iconUrl: redMarker.src,
+    iconSize: [36, 36],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -41],
+    shadowUrl: "",
+  }), []);
 
   return (
     <div className="map-div">
       <MapContainer className="map" center={[centeredLoc[0].lat, centeredLoc[0].lng]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-        <Marker
-          ref={markerRef}
-          position={[centeredLoc[0].lat, centeredLoc[0].lng]}
-          eventHandlers={eventHandlers}
-        >
-        <Popup>
-          {centeredLoc[0].name}
-        </Popup>
-      </Marker>
-    </MapContainer>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Clusters allLocs={allLocs} />
+        <CenteredMarker centeredLoc={centeredLoc} icon={centeredLocIcon} />
+      </MapContainer>
     </div>
-  )
-}
+  );
+};
 
 export default LeafletMap;
